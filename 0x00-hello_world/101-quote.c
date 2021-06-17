34d67858103c5201976b66688de43cc24d6e3f6f
Norman Munge <munge@sisitech.com>
2021-06-17 22:05:14 +0300

    Command that prints out the last part of quote in standard error:

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main-Prints out the last part of quote in std error
+* Return: 1 if success
+*/
+
+int main(void)
+{
+write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+return(1);
+}
