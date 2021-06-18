4f21f7add065b11900b150b56dfdd49e3f417cc0
Norman Munge <munge@sisitech.com>
2021-06-18 15:38:43 +0300

    Program to show conditions in C

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,14 +14,14 @@ n = rand() - RAND_MAX / 2;
 /* your code goes there */
 if (n > 0) 
 {
-    printf("is positive\n");
+    printf("%d is positive\n", n);
 }
 else
 {
     if (n < 0)
-        printf("is negative\n");
+        printf("%d is negative\n", n);
     else
-        printf("is zer0\n");
+        printf("%d is zer0\n", n);
 }
 return (0);
 }
