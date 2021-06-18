411de538b17a2a44393f9504f981999f98cf5cff
Norman Munge <munge@sisitech.com>
2021-06-18 16:03:47 +0300

    Adds program conditionals in c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,13 +14,14 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 /* your code goes there */
+int lastDigit = n%10;
 if (n > 5)
-printf("Last digit of %d is greater than 5\n", n);
+printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
 else
 if (n < 6 && n != 0)
-printf("Last digit of %d is less than 6 and not 0\n", n);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
 else
 if (n == 0)
-printf("Last digit of %d is 0\n", n);
+printf("Last digit of %d is %d and is 0\n", n, lastDigit);
 return (0);
 }
