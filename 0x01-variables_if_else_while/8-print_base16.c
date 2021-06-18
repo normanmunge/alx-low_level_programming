aa7ba0e738ce43d2dc4b4b315ba54226c0836253
Norman Munge <munge@sisitech.com>
2021-06-18 17:58:23 +0300

    Prints all the hexadecimal in lower

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main-Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char hexadec = "0123456789ABCDEF";
+putchar(tolower(hexadec));
+putchar("\n");
+return (0);
+}
