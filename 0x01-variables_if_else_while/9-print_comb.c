a603b343e088795409efb6329e73954978a0d537
Norman Munge <munge@sisitech.com>
2021-06-18 18:30:23 +0300

    Adds program to print possible combinations of single digits

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main-Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i = 0;
+while (i <= 9)
+{
+putchar(i + '0');
+if(i != 9)
+{
+putchar(',');
+putchar(' ');
+}
+i++;
+}
+return (0);
+}
+
