0d3fc4be6a3f6c907333a017001cadfd3f2e9fad
Norman Munge <munge@sisitech.com>
2021-06-18 15:41:13 +0300

    Program to show conditions in C

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,9 +2,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/*
+/* main-Entry point
+/* if condition statements in c
+ */
 int main(void)
 {
 int n;
@@ -12,16 +13,12 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 /* your code goes there */
-if (n > 0) 
-{
+if (n > 0)
     printf("%d is positive\n", n);
-}
 else
-{
     if (n < 0)
         printf("%d is negative\n", n);
     else
-        printf("%d is zer0\n", n);
-}
+        printf("%d is zero\n", n);
 return (0);
 }
