7efa25af72b995611bfd037f7184c867cefe705e
Norman Munge <munge@sisitech.com>
2021-06-22 18:55:36 +0300

    Updates the program to print alphabets in lowercase:

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+* main - Entry point
+*
+* Description: Program to print alphabet in lowercase followed by new line.
+* Return: Always 0 (Success)
+*/
+void print_alphabet(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return;
+}
+
+int main(void)
+{
+print_alphabet();
+return (0);
+}
