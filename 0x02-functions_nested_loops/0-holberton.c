b76b80078faa75c249e9fcda5a42887acc01ca45
Norman Munge <munge@sisitech.com>
2021-06-22 18:19:59 +0300

    Creates holberton header file:

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -2,12 +2,20 @@
 #include "holberton.h"
 /**
 * main-Entry point
+*
 * Return Always 0 (Success)
 */
 
-int main(void) {
+int main(void)
+{
+int i = 0;
 char *holberton = "Holberton";
+while(*holberton > 0)
+{
 putchar(*holberton);
+i++;
+}
+putchar(*holberton++);
 putchar('\n');
 return (0);
 }
