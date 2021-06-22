67d8b68ad003fdcb19e5e3c47df7c10327de28ac
Norman Munge <munge@sisitech.com>
2021-06-22 19:29:06 +0300

    Updates program to print alphabets in lowercase 10 times:

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+* print_alphabet - Function to print alphabet
+*
+* Description: Program to print alphabet in lowercase followed by new line.
+* Return: Always 0 (Success)
+*/
+
+void print_alphabet_x10(void)
+{
+char ch;
+int i = 0;
+while(i <=10 )
+{
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+i++;
+}
+return;
+}
