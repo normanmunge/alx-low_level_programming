2c25025720afced38b045a79d7987bb5620b6956
Norman Munge <munge@sisitech.com>
2021-06-22 19:10:35 +0300

    Updates program to print funciton that calls alphabet in lower case:

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,14 +7,6 @@
 * Return: Always 0 (Success)
 */
 
-void print_alphabet();
-
-int main(void)
-{
-print_alphabet();
-return (0);
-}
-
 void print_alphabet(void)
 {
 char ch;
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void);
+
+int main(void)
+{
+    print_alphabet();
+    return (0);
+}
