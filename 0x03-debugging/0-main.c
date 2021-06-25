6d8b314419fe747fadf46eda8623cd310e3cbf51
Norman Munge <munge@sisitech.com>
2021-06-25 19:27:42 +0300

    Updates the 0-main file

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -8,10 +8,10 @@
 */
 void positive_or_negative(int i)
 {
-int i;
+// int i;
 
-srand(time(0));
-i = rand() - RAND_MAX / 2;
+// srand(time(0));
+// i = rand() - RAND_MAX / 2;
 /* your code goes there */
 if (i > 0)
 printf("%d is positive\n", i);
